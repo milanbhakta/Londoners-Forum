e03163cdf1dbff2dc3e52385f00ba904181b9e18
Milan Bhakta <milzbhakta@gmail.com>
2019-10-17 09:50:41 -0400

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -3,12 +3,7 @@
 
 
 <?php
-/**
- * Created by PhpStorm.
- * User: Pedro 
- * Date: 18/02/2019
- * Time: 22:14
- */
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'londoners');
 define('DB_PASSWORD', 'London123!');
